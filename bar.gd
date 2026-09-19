b5fe7b27994776d8ad6dc2e6f3d0fb52b84493ba
ImBoyYoGG <109167179+ImBoyYoGG@users.noreply.github.com>
2026-09-19 20:28:04 +0300

    Update bar.gd

--- bar.gd
+++ bar.gd
@@ -1,10 +1,9 @@
 extends Control
 var taste_list :=[]
 const sweet := "sweet"
-var sweet_prec :float=0.5
+var sweet_prec :float=0
 const sour := "sour"
-var sour_prec :float=0.5
-
+var sour_prec :float=0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$HBoxContainer/sour.size_flags_stretch_ratio=0
@@ -16,25 +15,20 @@ func add_prec(index,taste,prec):
 	if index==taste:
 			prec+=1/len(taste_list)
 func _process(delta: float) -> void:
-	sweet_prec=0
-	sour_prec=0
-	
-	for i in taste_list:
-		if i == "sweet":
-			sweet_prec+=1/float(len(taste_list))
-		if i == "sour":
-			sour_prec+=1/float(len(taste_list))
-	
-		
-	if len(taste_list)!=0:
-		$HBoxContainer/Panel.size_flags_stretch_ratio=0
-	else:
+	if len(taste_list) == 0:
+		sweet_prec = 0
+		sour_prec = 0
 		$HBoxContainer/Panel.size_flags_stretch_ratio=1
-
-	if sweet_prec!=0:
+	else:
+		$HBoxContainer/Panel.size_flags_stretch_ratio=0
+		sweet_prec = str(taste_list).count("sweet") / float(len(taste_list))
+		sour_prec = str(taste_list).count("sour") / float(len(taste_list))
 		$HBoxContainer/sweet.size_flags_stretch_ratio=sweet_prec
-	if sour_prec!=0:
 		$HBoxContainer/sour.size_flags_stretch_ratio=sour_prec
+		
+		
+		
+		
 func _on_sour_button_pressed() -> void:
 	taste_list.append(sour)
 	
