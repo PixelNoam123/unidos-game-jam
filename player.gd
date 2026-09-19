772d7b8d2bd099897d1a84ebf82a9d9a3e05840e
PixelNoam123 <noamshaltiel2012@gmail.com>
2026-09-19 11:24:24 +0300

    basic player controll

--- player.gd
+++ player.gd
@@ -1,7 +1,9 @@
 extends CharacterBody2D
 var xsp :float = 0
 var ysp :float = 0
-var gravity := 1000
+var gravity := 100
+var speed :float =300
+var jump_power :float =2000
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,7 +13,11 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	ysp+=gravity
-	xsp=(int(Input.is_action_pressed("right"))-int(Input.is_action_pressed("left")))
+	if is_on_floor():
+		ysp=0
+		if Input.is_action_just_pressed("jump"):
+			ysp=-jump_power
+	xsp=(int(Input.is_action_pressed("right"))-int(Input.is_action_pressed("left")))*speed
 	velocity = Vector2(xsp,ysp)
 	move_and_slide()
 	
