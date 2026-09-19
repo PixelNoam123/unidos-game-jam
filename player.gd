d9fc10d89ad43800da77e0cd7370343301437ba5
ImBoyYoGG <109167179+ImBoyYoGG@users.noreply.github.com>
2026-09-19 12:48:44 +0300

    will it work?? player.gd jump

--- player.gd
+++ player.gd
@@ -1,9 +1,9 @@
 extends CharacterBody2D
 var xsp :float = 0
 var ysp :float = 0
-var gravity :=  500000
-var speed :float =30000
-var jump_power :float = 200000
+var gravity :=  5000
+var speed :float = 30000
+var jump_power :float = 1000
 var can_get_jump_power :bool = true
 
 
@@ -21,7 +21,7 @@ func _process(delta: float) -> void:
 	if is_on_floor():
 		ysp=0
 		if Input.is_action_pressed("jump"):
-			ysp=-jump_power
+			ysp = -jump_power
 	if is_on_ceiling():
 		ysp=0
 	#as long as you keep holding jump and you dont reach the limit the jump power keeps getting apllied
@@ -30,7 +30,7 @@ func _process(delta: float) -> void:
 	#applies gravity
 	ysp+=gravity*delta
 	#calculates x movement
-	xsp=(int(Input.is_action_pressed("right"))-int(Input.is_action_pressed("left")))*speed
+	xsp=(int(Input.is_action_pressed("right"))-int(Input.is_action_pressed("left")))*speed *delta
 	#godot defult characterbody2d velocity - applies delta automaticly and doesnt let the player go through staticbody2d nodes
-	velocity = Vector2(xsp,ysp)*delta
+	velocity = Vector2(xsp,ysp)
 	move_and_slide()
