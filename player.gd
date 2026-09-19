6bf81f75b50d3bda4410740164415e0478096a5a
PixelNoam123 <noamshaltiel2012@gmail.com>
2026-09-19 11:55:20 +0300

    almost finished player movement

--- player.gd
+++ player.gd
@@ -1,9 +1,12 @@
 extends CharacterBody2D
 var xsp :float = 0
 var ysp :float = 0
-var gravity := 100
+var gravity := 70
 var speed :float =300
-var jump_power :float =2000
+var jump_tick_max :=20
+var jump_tick := 0
+var jump_power :float = 1000
+var can_get_jump_power :bool = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,12 +15,25 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	ysp+=gravity
+	#if one floor all the jumping things reset
 	if is_on_floor():
+		can_get_jump_power=true
 		ysp=0
-		if Input.is_action_just_pressed("jump"):
-			ysp=-jump_power
+		jump_tick=0
+	#as long as you keep holding jump and you dont reach the limit the jump power keeps getting apllied
+	if Input.is_action_pressed("jump") and can_get_jump_power:
+		ysp=-jump_power
+		jump_tick+=1
+		if jump_tick>=jump_tick_max:
+			can_get_jump_power=false
+	#sets when the jump holding finishes
+	elif not (Input.is_action_pressed("jump") and can_get_jump_power):
+		can_get_jump_power=false
+	#applies gravity
+	ysp+=gravity
+	#calculates x movement
 	xsp=(int(Input.is_action_pressed("right"))-int(Input.is_action_pressed("left")))*speed
+	#godot defult characterbody2d velocity - applies delta automaticly and doesnt let the player go through staticbody2d nodes
 	velocity = Vector2(xsp,ysp)
 	move_and_slide()
 	
