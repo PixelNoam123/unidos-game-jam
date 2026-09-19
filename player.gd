7f247ce014609f7809da649dadee6f1ffd010cd5
PixelNoam123 <noamshaltiel2012@gmail.com>
2026-09-19 11:16:40 +0300

    basic player movement

--- player.gd
+++ player.gd
@@ -1,5 +1,7 @@
-extends Node2D
-
+extends CharacterBody2D
+var xsp :float = 0
+var ysp :float = 0
+var gravity := 1000
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,4 +10,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	ysp+=gravity
+	xsp=(int(Input.is_action_pressed("right"))-int(Input.is_action_pressed("left")))
+	velocity = Vector2(xsp,ysp)
+	move_and_slide()
+	
