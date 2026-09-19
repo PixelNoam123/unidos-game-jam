b992cfab40fe4b0ab6b0fca3341359fd8abc0ebe
PixelNoam123 <noamshaltiel2012@gmail.com>
2026-09-19 12:32:32 +0300

    back fix

--- player.gd
+++ player.gd
@@ -21,7 +21,7 @@ func _process(delta: float) -> void:
 	if is_on_floor():
 		ysp=0
 		if Input.is_action_pressed("jump"):
-			ysp-=jump_power
+			ysp=-jump_power
 	if is_on_ceiling():
 		ysp=0
 	#as long as you keep holding jump and you dont reach the limit the jump power keeps getting apllied
@@ -32,5 +32,5 @@ func _process(delta: float) -> void:
 	#calculates x movement
 	xsp=(int(Input.is_action_pressed("right"))-int(Input.is_action_pressed("left")))*speed
 	#godot defult characterbody2d velocity - applies delta automaticly and doesnt let the player go through staticbody2d nodes
-	velocity = Vector2(xsp,ysp)*delta
+	velocity = Vector2(xsp,ysp)
 	move_and_slide()
